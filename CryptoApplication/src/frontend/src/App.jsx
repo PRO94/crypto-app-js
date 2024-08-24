bf7cc421ba396a12ad08d5872446b55c5f1b2f9e
Roman Prytulenko <roman.prytulenko@gmail.com>
2024-08-24 18:07:07 +0200

    Add basic page structure with Header, Sider and Content components. Use antd 5.20.2 library for UI.

--- CryptoApplication/src/frontend/src/App.jsx
+++ CryptoApplication/src/frontend/src/App.jsx
@@ -1,3 +1,16 @@
+import { Layout } from 'antd';
+import AppHeader from './components/layout/AppHeader';
+import AppSider from './components/layout/AppSider';
+import AppContent from './components/layout/AppContent';
+
 export default function App() {
-  return <h1>React Crypto App</h1>
+  return (
+    <Layout>
+      <AppHeader></AppHeader>
+      <Layout>
+        <AppSider></AppSider>
+        <AppContent></AppContent>
+      </Layout>
+    </Layout>
+  )
 }
