436b46e83b16c6d4c286cd63fb2c1b973b7efed1
Roman Prytulenko <roman.prytulenko@gmail.com>
2024-08-30 12:56:11 +0200

    Add micro backend to run a server

---
+++ CryptoApplication/src/backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 80;
+
+app.use(express.static("../frontend/dist"));
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// Test the server:
+// http://localhost:80
